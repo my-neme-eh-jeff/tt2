4e583ac026446822fd983240aed54b5d94230d70
my-neme-eh-jeff <aman2003nambisan@gmail.com>
2022-12-10 21:33:36 +0530

    yes

--- binarySearch.c
+++ binarySearch.c
@@ -3,6 +3,22 @@
 
 int x;
 
+
+bool binary(int a[], int l, int r,int x){
+    if(l>r){
+        return false;
+    }
+    int mid = (l+r)/2;
+
+    if(a[mid]<x){
+        binary(a,mid+1,r,x);
+    }else if(a[mid]>x){
+        binary(a,l,mid-1,x);
+    }else{
+        return true;
+    }
+}
+
 void main(){
     int a[100];
     int n;
@@ -13,20 +29,10 @@ void main(){
     }
     printf("Enter the value to be searched");
     scanf("%d",&x);    
-    bool flag = binary(a,0,n-1,x)
-}
-
-bool binary(int a[], int l, int r,int x){
-    if(l>r){
-        return ;
-    }
-    int mid = (l+r)/2;
-
-    if(a[mid]<x){
-        binary(a,mid+1,x);
-    }else if(a[mid>x]){
-        binary(a,l,mid-1,x);
+    bool flag = binary(a,0,n-1,x);
+    if(flag==true){
+        printf("NUMBER HAS FOUND");
     }else{
-        return true;
+        printf("NUMBER HAS NOT BEEN FOUND");
     }
 }
